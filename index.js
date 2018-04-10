88811401fa97640049f9cce3a71a0ace50f632de
Phoebe Lau <phoebelau7@gmail.com>
2018-04-10 01:48:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ function searchRepositories() {
   let url = `https://api.github.com/search/repositories?q=${searchTerms}`;
   $.get(url).done(function(response) {
     const results = response.items.map(r => { return (
-      `<strong><a href="${r.html_url}" target="_blank">${r.name}</a></strong>
-      <p>${r.description}</p>
-      <a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a><br>
-      <img src="${r.owner.avatar_url}" height="32" width="32"/><br>
-      <a href="${r.owner.url}" target="_blank">${r.owner.login}</a>`
+      `<strong><a href="${r.html_url}" target="_blank">${r.name}</a></strong><br/>
+      <em>${r.description}</em><br />
+      <small><a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></small>
+      <p></p>
+      <img src="${r.owner.avatar_url}" height="32" width="32"/><br />
+      <a href="${r.owner.url}" target="_blank">${r.owner.login}</a>
+      <hr>`
     )})
     document.getElementById("results").innerHTML = results
   }).fail(error => {
@@ -29,9 +31,11 @@ function showCommits(el) {
   const url = `https://api.github.com/repos/${username}/${repo}/commits`
   $.get(url).done(function(response) {
     let commits = response.map(c => { return (
-      `${c.sha} | ${c.commit.message}
+      `${c.sha}<br />
+      ${c.commit.message}</p>
       <img src="${c.author.avatar_url}" height="32" width="32"/><br/>
-      By ${c.commit.author.name} <a href="${c.author.url}" target="_blank">${c.author.login}</a>`
+      By ${c.commit.author.name} (<a href="${c.author.url}" target="_blank">${c.author.login}</a>)
+      <hr>`
     )});
     document.getElementById("details").innerHTML = commits
   }).fail(error => {
