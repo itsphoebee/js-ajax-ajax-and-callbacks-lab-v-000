62f9ebc7a48c92d60d13a8436ec05f13411a8542
Phoebe Lau <phoebelau7@gmail.com>
2018-04-10 01:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function searchRepositories() {
       `<h2><a href="${r.html_url}">${r.name}</a></h2>
       <p><a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
       <p>${r.description}</p>
-      <img src="${r.owner.avatar_url}" height="32" width="32"/><br>
-      <a href="${r.owner.url}" target="_blank">${r.owner.login}</a>`
+      `
     )})
     document.getElementById("results").innerHTML = results
   }).fail(error => {
